11b96784cf6c403a4e9f5e1bcb71b6a952342497
irshadpukayoor <irshadpukayoor@gmail.com>
2025-11-19 06:34:06 +0000

    fix: implement GTM in _document for verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,7 @@ import type { Metadata } from "next";
 import { Poppins, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SITE_CONFIG } from "@/lib/constants";
-import Script from "next/script"; // <-- REQUIRED IMPORT
 import ContactModal from "@/components/Modal/ContactModal";
-
-// Define GTM ID once
-const GTM_ID = "GTM-WTTZD4MF"; 
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -62,40 +58,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        {/* Google Tag Manager - Main Script (MUST be in <head> for verification) */}
-        <Script
-          id="google-tag-manager-script"
-          // 'beforeInteractive' ensures the script is included in the initial HTML
-          strategy="beforeInteractive" 
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager - Main Script */}
-      </head>
-
       <body
         className={`${poppins.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
-        {/* Google Tag Manager (noscript) - MUST be immediately after opening <body> tag */}
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
         {children}
         <ContactModal />
       </body>
