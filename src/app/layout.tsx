4f4c210cb7eeb8ffaa1453a01d7b213a89e1477c
IrshadPeruvallur <irshadpukayoor@gmail.com>
2025-11-20 10:32:29 +0530

    google ad manager script updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 // app/layout.tsx
-import { GoogleTagManager } from '@next/third-parties/google';
 import type { Metadata } from "next";
 import { Poppins, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SITE_CONFIG } from "@/lib/constants";
-import ContactModal from "@/components/Modal/ContactModal";
-import WhatsAppWidget from "@/components/WhatsAppWidget";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -22,6 +19,9 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_CONFIG.url),
+  verification: {
+    google: "FEEPMTuIlMk8-qSGivtt8URG6yT_vwXRN_-2v1ZQzRY",
+  },
   title: {
     template: "%s | Diginext",
     default: "IT Solutions and Services Company in Dubai | Diginext",
@@ -60,15 +60,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WTTZD4MF');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+
       <body
         className={`${poppins.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WTTZD4MF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {children}
-        <ContactModal />
-        <WhatsAppWidget />
       </body>
-      <GoogleTagManager gtmId="GTM-5F46C6W5" />
     </html>
   );
 }
