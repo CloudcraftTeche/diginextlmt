a97d8c6179517373caef5495c68018f0f00544d7
IrshadPeruvallur <irshadpukayoor@gmail.com>
2025-12-05 11:07:21 +0530

    font alingment

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,9 +1,13 @@
 import { ApiConstants } from "@/constants/apiConstants";
 import apiClient from "@/lib/axiosInstance";
-import { LeadPayload } from "@/types/lead.types";
+import { LeadPayload, SubscribePayload } from "@/types/api.types";
 
 export const apiService = {
   createLead: (data: LeadPayload) => {
     return apiClient.post(ApiConstants.contact_leads_create, data);
   },
+  createSubscribe:(data:SubscribePayload)=>{
+    return apiClient.post(ApiConstants.api_subscribe_create, data);
+
+  }
 };
