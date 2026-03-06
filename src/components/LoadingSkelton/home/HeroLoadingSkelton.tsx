0edf9c0665f521f9b29391c2f84a02c8a192c5aa
IrshadPeruvallur <irshadpukayoor@gmail.com>
2026-03-06 22:37:29 +0530

    hero size

--- src/components/LoadingSkelton/home/HeroLoadingSkelton.tsx
+++ src/components/LoadingSkelton/home/HeroLoadingSkelton.tsx
@@ -13,7 +13,7 @@ export const HeroLoadingSkelton: React.FC = () => {
     >
       <div className="w-full bg-black via-black to-gray-800 rounded-xl xs:rounded-2xl p-3 xs:p-4 sm:p-6 lg:p-10 relative overflow-hidden">
         <div className={CONTENT_WRAPPER_CLASSES}>
-          <div className="flex flex-col lg:flex-row gap-6 xs:gap-8 sm:gap-10 lg:gap-8 xl:gap-10 2xl:gap-12 items-start lg:items-center min-h-[250px] xs:min-h-[300px] sm:min-h-[350px] lg:min-h-[420px]">
+          <div className="flex flex-col lg:flex-row gap-6 xs:gap-8 sm:gap-10 lg:gap-8 xl:gap-10 2xl:gap-12 items-start lg:items-center min-h-[250px] xs:min-h-[300px] sm:min-h-[350px] lg:min-h-[420px] 2xl:min-h-[800px]">
             {/* Loading skeleton for image */}
             <div className="w-full lg:w-5/12 flex justify-center lg:justify-start items-center">
               <div className="relative w-full max-w-[300px] lg:max-w-[400px] aspect-square bg-gray-800 rounded-lg animate-pulse" />
